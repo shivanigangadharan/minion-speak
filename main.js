3f8fd16454e87f900f9c4262945f0e8dba68fbf4
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-08-11 19:09:20 +0530

    feat: done translation

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var outputText = document.getElementById('output');
 var inputText = document.getElementById('english');
 
+function constructURL(text) {
+    return "https://api.funtranslations.com/translate/minion.json?text=" + text;
+}
+
 function readBanana(englishText) {
-    console.log('Text = ', englishText);
-    outputText.innerText = englishText;
+    fetch(constructURL(englishText))
+        .then(response => response.json())
+        .then(json => {
+            outputText.innerText = json.contents.translated;
+        }
+        )
+        .catch((err) => {
+            console.log('Sorry! There is some error with the server, please try again after an hour. The server is limited to only 5 requests per hour.');
+            alert("Sorry, the server is limited to only 5 requests per hour, please try again after a while. Thank you for understanding.");
+        });
 }
